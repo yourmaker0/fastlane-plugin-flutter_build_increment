ab809d34a520f0a9e549dd8ffbe64e5be7f22b71
yourmaker0 <40912672+yourmaker0@users.noreply.github.com>
2022-01-14 00:05:20 +0600

    +

--- lib/fastlane/plugin/flutter_build_increment/actions/flutter_build_increment_action.rb
+++ lib/fastlane/plugin/flutter_build_increment/actions/flutter_build_increment_action.rb
@@ -6,7 +6,7 @@ module Fastlane
     class FlutterBuildIncrementAction < Action
       def self.run(params)
         UI.message("The flutter_build_increment plugin is working!")
-        new_version_code ||= params[:version_code]
+        new_version_code ||= params.first
         file_name = '../pubspec.yaml'
         text = File.read(file_name)
         new_contents = text.gsub(/^(version:\s*\d+.\d+.\d+\+)(\d+)/) { |m| m.gsub!($2) {|num| new_version_code}  }  
